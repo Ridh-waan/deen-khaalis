35cd20684dd27228abbb8702c11fb9592dc22965
Maro Ridhiwani <maroridhiwani@gmail.com>
2023-09-26 19:27:48 +0300

    my second react responsive

--- web/src/Home.js
+++ web/src/Home.js
@@ -2,25 +2,26 @@ import React, { useEffect, useState } from 'react';
 import './index.css';
 import { motion } from 'framer-motion';
 import './App.css';
+import { useMediaQuery } from 'react-responsive';
 
 const Home = () => {
-  const [currentImage, setCurrentImage] = useState(0); // Initialize with 0
+  const [currentImage, setCurrentImage] = useState(0);
   const imageList = [
     { id: 1, text: '', imageName: 'Slide_DeenKhaalis_Binisa.jpg' },
     { id: 2, text: '', imageName: 'Slide_DeenKhaalis_Nurein.jpg' },
     { id: 3, text: '', imageName: 'academyprograms.png' },
-
   ];
+
+  const isMobileView = useMediaQuery({ maxWidth: 600 });
 
   useEffect(() => {
     const interval = setInterval(() => {
-      // Calculate the next image index in a cyclic manner
       const nextImageIndex = (currentImage + 1) % imageList.length;
       setCurrentImage(nextImageIndex);
-    }, 20000); // Change images every 2 minutes (120,000 milliseconds)
+    }, 20000);
 
     return () => {
-      clearInterval(interval); // Clean up the interval when the component unmounts
+      clearInterval(interval);
     };
   }, [currentImage, imageList]);
 
@@ -30,13 +31,16 @@ const Home = () => {
       <div className='image-container'>
         <div className='image-text-container'>
           <motion.img
-            src={`/images/${imageList[currentImage].imageName}`} // Use currentImage directly
+            src={`/images/${imageList[currentImage].imageName}`}
             alt='logo'
-            initial={{ x: -1000, scale: 0.5 }} // Initial position off-screen to the left and smaller size
-            animate={{ x: 0, scale: 1 }} // Animation target position (center) and size
-            transition={{ duration: 2, ease: 'easeInOut' }} // Animation duration and easing
+            initial={{ x: -1000, scale: 0.5 }}
+            animate={{ x: 0, scale: 1 }}
+            transition={{ duration: 2, ease: 'easeInOut' }}
+            style={{ width: isMobileView ? '100%' : '1250px' }}
           />
-          <p className="image-text">{imageList[currentImage].text}</p>
+          <p className="image-text" style={{ fontSize: isMobileView ? '16px' : '24px' }}>
+            {imageList[currentImage].text}
+          </p>
         </div>
       </div>
     </div>
@@ -44,3 +48,9 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
+
+
+
