f8a9b987be50c32c73680506b1c03ed92ef5735e
ABUJARIRY123 <maroridhiwani@gmail.com>
2024-03-18 10:50:25 +0300

    muharar updated

--- web/src/Duruus/BinIsa/Audios/hadeeth/muharar/Data.js
+++ web/src/Duruus/BinIsa/Audios/hadeeth/muharar/Data.js
@@ -8,6 +8,10 @@ export const muharardata = [
         "url": "https://ia801309.us.archive.org/24/items/05_20240212_202402/%5B02%5D.mp3"
     },
     {
+        "title": "Darsa ya tatu",
+        "url": "https://archive.org/download/03_20240318/%5B03%5D.mp3"
+    },
+    {
         "title": "Darsa ya nne",
         "url": "https://ia601309.us.archive.org/24/items/05_20240212_202402/%5B04%5D.mp3"
     },
