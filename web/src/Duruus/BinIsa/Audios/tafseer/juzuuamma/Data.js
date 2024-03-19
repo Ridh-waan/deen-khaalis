26965644ff514b4af75a080aaaff8a87d10f2455
ABUJARIRY123 <maroridhiwani@gmail.com>
2024-03-19 19:45:59 +0300

    laily

--- web/src/Duruus/BinIsa/Audios/tafseer/juzuuamma/Data.js
+++ web/src/Duruus/BinIsa/Audios/tafseer/juzuuamma/Data.js
@@ -56,7 +56,7 @@ export const fadhlulmadinadat = [
     },
     {
     "title": "Suratu Mutwafifiin aya 1-6",
-    "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B15%5D%20Surah%20Al-Mutaffifin%20%28Ayah%201-6%29.png",
+    "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B15%5D%20Surah%20Al-Mutaffifin%20%28Ayah%201-6%29.mp3",
     },
     {
         "title": "Suratu Mutwafifiin aya 7-17",
@@ -65,6 +65,109 @@ export const fadhlulmadinadat = [
     {
         "title": "Suratu Mutwafifiin aya 18-28",
         "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B17%5D%20Surah%20Al-Mutaffifin%20%28Ayah%2018-28%29.mp3",
+    },
+    {
+        "title": "Suratu Mutwafifiin aya 29-36",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B18%5D%20Surah%20Al-Mutaffifin%20%28Ayah%2029-36%29.mp3",
+    },
+    {
+        "title": "Suratu Inshiqaaq aya 1-6",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B19%5D%20Surah%20Al-Inshiqaq%20%28Ayah%201-6%29.mp3",
+    },
+    {
+        "title": "Suratu Inshiqaaq aya 7-19",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B20%5D%20Surah%20Al-Inshiqaq%20%28Ayah%207-19%29.mp3",
+    },
+    {
+        "title": "Suratu Inshiqaaq aya 20-25",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B21%5D%20Surah%20Al-Inshiqaq%20%28Ayah%2020-25%29.mp3",
+    },
+    {
+        "title": "Suratul Buruuj aya 1-8",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B22%5D%20Surah%20Al-Buruj%20%28Ayah%201-8%29.mp3",
+    },
+    {
+        "title": "Suratul Buruuj aya 9-11",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B07%5D%20Surah%20An-Nazi%27at%20%28Ayah%2027-41%29.mp3",
+    },
+    {
+        "title": "Suratul Buruuj aya 12-22",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B24%5D%20Surah%20Al-Buruj%20%28Ayah%2012-22%29.mp3",
+    },
+    {
+        "title": "Surat At-Taariq aya 1-8",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B25%5D%20Surah%20At-Tariq%20%28Ayah%201-8%29.mp3",
+    },
+    {
+        "title": "Surat At-Taariq aya 9-17",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B26%5D%20Surah%20At-Tariq%20%28Ayah%209-17%29.mp3",
+    },
+    {
+        "title": "Suratu 'Al A'ala aya 1-7",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B27%5D%20Surah%20Al-A%27la%20%28Ayah%201-7%29.mp3",
+    },
+    {
+        "title": "Suratu 'Al A'ala aya 8-13",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B28%5D%20Surah%20Al-A%27la%20%28Ayah%208-13%29.mp3",
+    },
+    {
+        "title": "Suratu 'Al A'ala aya 14-19",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B29%5D%20Surah%20Al-A%27la%20%28Ayah%2014-19%29.mp3",
+    },
+    {
+        "title": "Suratu Al Ghaashiyah aya 1-7",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B30%5D%20Surah%20Al-Ghashiyah%20%28Ayah%201-7%29.mp3",
+    },
+    {
+        "title": "Suratu Al Ghaashiyah aya 8-16",
+    "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B31%5D%20Surah%20Al-Ghashiyah%20%28Ayah%208-16%29.mp3",
+    },
+    {
+        "title": "Suratu Al Ghaashiyah aya 17-26",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B32%5D%20Surah%20Al-Ghashiyah%20%28Ayah%2017-26%29.mp3",
+    },
+    {
+        "title": "Suratul Fajir aya 1-9",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B33%5D%20Surah%20Al-Fajr%20%28Ayah%201-9%29.mp3",
+    },
+    {
+        "title": "Suratul Fajir aya 10-14",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B34%5D%20Surah%20Al-Fajr%20%28Ayah%2010-14%29.mp3",
+    },
+    {
+        "title": "Suratu Fajir aya 15-20",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B35%5D%20Surah%20Al-Fajr%20%28Ayah%2015-20%29.mp3",
+    },
+    {
+        "title": "Suratu Fajir aya 21-30",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B36%5D%20Surah%20Al-Fajr%20%28Ayah%2021-30%29.mp3",
+    },
+    {
+        "title": "Suratul Balad aya 1-10",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B37%5D%20Surah%20Al-Balad%20%28Ayah%201-10%29.mp3",
+    },
+    {
+        "title": "Suratul Balad aya 11-20",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B38%5D%20Surah%20Al-Balad%20%28Ayah%2011-20%29.mp3",
+    },
+    {
+        "title": "Suratu As-Shamsi aya 1-7",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B39%5D%20Surah%20Ash-Shams%20%28Ayah%201-7%29.mp3",},
+    {
+        "title": "Suratu As-Shamsi aya 8-15",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B40%5D%20Surah%20Ash-Shams%20%28Ayah%208-15%29.mp3",
+    },
+    {
+        "title": "Suratu Al-Laily aya 1-7",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B41%5D%20Surah%20Al-Layl%20%28Ayah%201-7%29.mp3",
+    },
+    {
+        "title": "Suratu Al-Laily aya 8-18",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B42%5D%20Surah%20Al-Layl%20%28Ayah%208-18%29.mp3",
+    },
+    {
+        "title": "Suratu Al-Laily aya 19-21",
+        "url": "https://archive.org/download/16-surah-al-mutaffifin-ayah-7-17/%5B43%5D%20Surah%20Al-Layl%20%28Ayah%2019-21%29.mp3",
     },
     
 ]
