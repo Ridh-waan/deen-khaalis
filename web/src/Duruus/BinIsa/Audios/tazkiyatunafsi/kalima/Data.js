3be9d9e8db2b0e7b3834cfc8f9e6e8f5ba597376
ABUJARIRY123 <maroridhiwani@gmail.com>
2024-03-20 13:59:08 +0300

    1.1

--- web/src/Duruus/BinIsa/Audios/tazkiyatunafsi/kalima/Data.js
+++ web/src/Duruus/BinIsa/Audios/tazkiyatunafsi/kalima/Data.js
@@ -9,6 +9,11 @@ export const malindidata = [
         "url": "https://archive.org/download/20240315_20240315_1247/Jibu_Kwa_Wanaosema_Kaburi_La_Mtume_Lipo_Ndani_Ya_Msikiti_Wake.mp3",
     },
     {
+        "title": "Kigogo chasikitika kumkosa Mtume  صلى الله عليه وسلم  ",
+        "url": "https://archive.org/download/20240315_20240315_1247/Kigogo%20Chasikitika%20Kumkosa%20Mtume%20%EF%B7%BA.mp4",
+    },
+
+    {
         "title": "Nasiha kuhusu vivyo vya 'Ulama",
         "url": "https://archive.org/download/20240315_20240315_1247/Nasiha%20Kuhusu%20Vifo%20Vya%20%27Ulam%C4%81.mp3",
     },
@@ -39,11 +44,11 @@ export const malindidata = [
    
     {
         "title": "Enyi mayahudi, tutawatoa Al-Aqsa, Nani atakayewatoa mayahudi Palestina",
-        "url": "",
+        "url": "https://archive.org/download/20240315_20240315_1247/Enyi%20Mayahudi%20Tutawatoa%20Al%20Aqsa%21%20Nani%20Atakayewatoa%20Mayahudi%20Palestina.mp4",
     },
     {
         "title": "Kubainisha utata katika kadhia ya Palestina na Hamasa",
-        "url": "",
+        "url": "https://archive.org/download/20240315_20240315_1247/%F0%9D%99%86%F0%9D%99%AA%F0%9D%99%97%F0%9D%99%96%F0%9D%99%9E%F0%9D%99%A3%F0%9D%99%9E%F0%9D%99%A8%F0%9D%99%9D%F0%9D%99%96%20%F0%9D%99%90%F0%9D%99%A9%F0%9D%99%96%F0%9D%99%A9%F0%9D%99%96%20%F0%9D%99%86%F0%9D%99%96%F0%9D%99%A9%F0%9D%99%9E%F0%9D%99%A0%F0%9D%99%96%20%F0%9D%99%86%F0%9D%99%96%F0%9D%99%99%F0%9D%99%9D%F0%9D%99%9E%F0%9D%99%96%20%F0%9D%99%AE%F0%9D%99%96%20%F0%9D%99%8B%F0%9D%99%96%F0%9D%99%A1%F0%9D%99%9A%F0%9D%99%A8%F0%9D%99%A9%F0%9D%99%9E%F0%9D%99%A3%EF%BF%BD%EF%BF%BD%EF%BF%BD.mp4",
     },
    
     {
@@ -51,32 +56,32 @@ export const malindidata = [
         "url": "https://archive.org/download/20240315_20240315_1247/%D8%AE%D8%B7%D8%B1%20%D8%A7%D9%84%D8%B5%D9%88%D9%81%D9%8A%D8%A9%20%D8%B9%D9%84%D9%89%20%D8%A7%D9%84%D8%A5%D8%B3%D9%84%D8%A7%D9%85.mp3",
     },
     {
-        "title": "Kalima ua ufunguzi",
+        "title": "Malengo ya kusoma",
         "url": "https://archive.org/download/20240315_20240315_1247/%D9%83%D9%84%D9%85%D8%A9%20%D8%A5%D9%81%D8%AA%D8%AA%D8%A7%D8%AD%D9%8A%D8%A9.mp3",
     },
     {
-        "title": "Kalima I",
+        "title": "Kalima masjid Imaam Bukhari",
         "url": "https://archive.org/download/20240315_20240315_1247/1KALIMAH.mp3",
     },
+   
     {
-        "title": "Kalima II",
-        "url": "https://archive.org/download/20240315_20240315_1247/KALIMAH.mp3",
-    },
-    {
-        "title": "Kalima III",
+        "title": "Kalima",
         "url": "https://archive.org/download/20240315_20240315_1247/kalima.mp3",
     },
     {
-        "title": "مظاهر الشرك في هذا العصر",
+        "title": "Kalima Rabai",
         "url": "https://archive.org/download/20240315_20240315_1247/%D9%85%D8%B8%D8%A7%D9%87%D8%B1%20%D8%A7%D9%84%D8%B4%D8%B1%D9%83%20%D9%81%D9%8A%20%D9%87%D8%B0%D8%A7%20%D8%A7%D9%84%D8%B9%D8%B5%D8%B1.mp3",
     },
     {
-        "title": "حقيقة  الصيام  وأنواعه.",
+        "title": "Kalima vipingo",
         "url": "https://archive.org/download/20240315_20240315_1247/%D8%AD%D9%82%D9%8A%D9%82%D8%A9%20%D8%A7%D9%84%D8%B5%D9%8A%D8%A7%D9%85%20%D9%88%D8%A3%D9%86%D9%88%D8%A7%D8%B9%D9%87.mp3",
     },
     {
-        "title": "لعبودية لله تعالى",
+        "title": "Muhadhara Masjid salaam",
         "url": "https://ia800203.us.archive.org/25/items/20240315_20240315_1247/%D8%A7%D9%84%D8%B9%D8%A8%D9%88%D8%AF%D9%8A%D8%A9%20%D9%84%D9%84%D9%87%20%D8%AA%D8%B9%D8%A7%D9%84%D9%89.mp3",
     },
+    {
+        "title": "Taqwah ya Allah",
+        "url": "https://archive.org/download/20240315_20240315_1247/%5B%F0%9D%95%84%F0%9D%95%A6%F0%9D%95%99%F0%9D%95%92%F0%9D%95%95%F0%9D%95%99%F0%9D%95%92%F0%9D%95%A3%F0%9D%95%92%5D-%F0%9D%90%93%F0%9D%90%9A%F0%9D%90%B0%F0%9D%90%9F%F0%9D%90%A2%F0%9D%90%AA%20%F0%9D%90%98%F0%9D%90%9A%20%F0%9D%90%80%F0%9D%90%A5%F0%9D%90%A5%F0%9D%90%9A%F0%9D%90%A1%20__%20%D8%A7%D9%84%D8%AA%D9%88%D9%81%D9%8A%D9%82%20%D9%88%D8%A3%D8%B3%D8%A8%D8%A7%D8%A8%D9%87%20__%20Shk.Muha.mp4",
+    },
 ]
-
