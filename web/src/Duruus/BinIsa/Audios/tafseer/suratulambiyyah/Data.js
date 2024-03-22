2e5cbb2bf549010b627516704e15cac3eb74eb3f
ABUJARIRY123 <maroridhiwani@gmail.com>
2024-03-22 15:50:31 +0300

    suratul ambiyaah 25

---
+++ web/src/Duruus/BinIsa/Audios/tafseer/suratulambiyyah/Data.js
@@ -0,0 +1,18 @@
+export const malindidata = [
+    {
+        "title": "Aya 1-4",
+        "url": "https://archive.org/download/01-ayah-1-4/%5B01%5D%20Ayah%201-4.mp3",
+    },
+    {
+        "title": "Aya 5-10",
+        "url": "https://archive.org/download/01-ayah-1-4/%5B02%5D%20Ayah%205-10.mp3",
+    },
+    {
+        "title": "Aya 11-18",
+        "url": "https://archive.org/download/01-ayah-1-4/%5B03%5D%20Ayah%2011-18.mp3",
+    },
+    {
+        "title": "Aya 19-25",
+        "url": "https://archive.org/download/01-ayah-1-4/%5B04%5D%20Ayah%2019-25.mp3",
+    },
+]
