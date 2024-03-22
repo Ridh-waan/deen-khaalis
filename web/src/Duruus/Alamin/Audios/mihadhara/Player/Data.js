365e530f8bbf8c481953b442397d05d8ed9d8a51
ABUJARIRY123 <maroridhiwani@gmail.com>
2024-03-22 09:40:11 +0300

    mihadhara ust alamin

--- web/src/Duruus/Alamin/Audios/mihadhara/Player/Data.js
+++ web/src/Duruus/Alamin/Audios/mihadhara/Player/Data.js
@@ -8,11 +8,39 @@ export const mihadharadata = [
         "url": "https://ia600204.us.archive.org/19/items/manhaj-ya-ahlus-sunnah-wal-jamma-ah/Ni%20malize%20taraweh%20na%20%20imam%20mgani.MP3.mp3",
     },
     {
+        "title": "Sifa za kipote kilichookoka",
+        "url": "https://archive.org/download/manhaj-ya-ahlus-sunnah-wal-jamma-ah/Sifa%20za%20Kipote%20Kilichookoka.mp3",
+    },
+    {
+        "title": "Umuhimu wa kuhudhuria vikao vya elimu",
+        "url": "https://archive.org/download/manhaj-ya-ahlus-sunnah-wal-jamma-ah/UMUHIMU%20WA%20KUHUDHURIA%20VIKAO%20VYA%20ILIMU.mp3",
+    },
+    {
         "title": "Kuwa mkweli na Allah",
         "url": "https://ia600204.us.archive.org/19/items/manhaj-ya-ahlus-sunnah-wal-jamma-ah/Kuwa%20mkweli%20na%20Allah.mp3",
     },{
         "title": "Manhaj ya Ahlu sunnah wal jamaa",
         "url": "https://ia600204.us.archive.org/19/items/manhaj-ya-ahlus-sunnah-wal-jamma-ah/MANHAJ%20YA%20AHLUS-SUNNAH%20WAL%20JAMM%C4%80%27AH.mp3",
     },
+    {
+        "title": "Fadhila ya Qur'aan",
+        "url": "https://archive.org/download/manhaj-ya-ahlus-sunnah-wal-jamma-ah/Fadhla%20ya%20Qur%27an.mp3",
+    },
+    {
+        "title": "Je huku sio katika kumkejeli Mtume صلى الله عليه وسلم?",
+        "url": "https://archive.org/download/manhaj-ya-ahlus-sunnah-wal-jamma-ah/Je%20huku%20sio%20katika%20kumkejeli%20mtume.mp3",
+    },
+    {
+        "title": "Kuchagua marafiki",
+        "url": "https://archive.org/download/manhaj-ya-ahlus-sunnah-wal-jamma-ah/Kuchagua%20Marafiki.mp3",
+    },{
+        "title": "Kumuadhimisha Allah na maamrisho yake",
+        "url": "https://archive.org/download/manhaj-ya-ahlus-sunnah-wal-jamma-ah/Kumuadhimisha%20All%C4%81h%20na%20Maamrisho%20yake.mp3",
+    },
+    {
+        "title": "Neema ya Allah juu yetu",
+        "url": "https://archive.org/download/manhaj-ya-ahlus-sunnah-wal-jamma-ah/Neema%20za%20Allah%20juu%20yetu.mp3",
+    },
+    
     
 ]
