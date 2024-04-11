e8e11786f564067256eb17991129fea66fbcbcac
ABUJARIRY123 <maroridhiwani@gmail.com>
2024-04-11 12:38:47 +0300

    counts

--- web/src/components/Visits.js
+++ web/src/components/Visits.js
@@ -1,27 +1,35 @@
-import React, { useState, useEffect } from 'react';
-
-const Visits = () => {
+import { useState, useEffect } from 'react';
+const Counts = () => {
+ const [pageviews, setPageviews] = useState(0);
  const [visits, setVisits] = useState(0);
 
  useEffect(() => {
-    const fetchVisitCount = async () => {
-      try {
-        const response = await fetch('/api/visits');
-        const data = await response.json();
-        setVisits(data.visits);
-      } catch (error) {
-        console.error('Failed to fetch visit count:', error);
-      }
-    };
-
-    fetchVisitCount();
+    const type = sessionStorage.getItem('visit') === null ? 'visit-pageview' : 'pageview';
+    updateCounter(type);
  }, []);
+
+ function updateCounter(type) {
+    fetch(`https://server-ney9.onrender.com/api?${type}`) // Adjusted port to match backend
+        // fetch(`// Adjusted port to match backend`) 
+      .then(res => res.json())
+      .then(data => {
+        setVisits(data.visits);
+        setPageviews(data.pageviews);
+        sessionStorage.setItem('visit', 'x');
+      });
+      console.log('Page views are:', pageviews)
+      console.log('Visits are:', visits)
+
+ }
+
+//  Divide pageviews by 2 and round down to the nearest whole number
+ const newPageviews = Math.floor(pageviews / 2);
 
  return (
     <div>
-      {/* <h1>Tumepata visits {visits} Alhamdulillah</h1> */}
+      <p> Kwa sasa tumepata {newPageviews} visits, Alhamdulillah</p>
     </div>
  );
 };
 
-export default Visits;
+export default Counts;
